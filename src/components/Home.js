636148b15fbfded14e20c2d45cbd43bc47c37c0f
github username <github email address>
2019-06-01 12:02:57 -0700

    create Home.js component, stub out dummy data

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home Page</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam molestias voluptatem saepe ut, asperiores numquam? Unde iure obcaecati eos maiores magnam hic, nesciunt eius aspernatur, est aut quis porro nisi.</p>
+    </div>
+  )
+}
+
+export default Home;
