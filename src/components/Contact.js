20cbcee66f150410f01849a6f12d7414b54e4fc6
github username <github email address>
2019-06-01 13:00:41 -0700

    add router props to Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Contact = () => {
+// props are from the router
+const Contact = (props) => {
   return (
     <div className="container">
       <h4 className="center">Contact Page</h4>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -7,6 +7,7 @@ const Navbar = () => {
       <div className="container">
         <a className="brand-logo">Brainstorm</a>
         <ul>
+          {/* Link to provides an async update to each of the routes */}
           <li><Link to="/">Home Page</Link></li>
           <li><Link to="/about">About Page</Link></li>
           <li><Link to="/contact">Contact Page</Link></li>
