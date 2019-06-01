23d4e09e96e3064152e2fa9044d94ae1ac5351cc
github username <github email address>
2019-06-01 11:52:59 -0700

    stub out Navbar imports on App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Ideas from './components/Ideas';
 import AddIdea from './components/AddIdea';
+import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+
 import './App.css';
 
 
