6ab7422b1a1625f7f7ee1772e81bb51b25d7440d
github username <github email address>
2019-06-01 15:49:01 -0700

    change delete button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home'
