1de1fad3194c8ee6e66e7d61327f0af3ca64b39d
github username <github email address>
2019-06-02 16:47:36 -0700

    fix idea layout and add link to idea.id

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchIdeas, deleteIdea } from '../actions/ideaActions';
 import Target from '../target.jpg';
+import { Link } from 'react-router-dom';
 
 class Ideas extends Component {
 
@@ -13,8 +14,10 @@ render() {
   const renderIdeas = this.props.ideas.concat().reverse().map(idea =>
     <div className="bucketlist card" key={idea.id}>
       <img src={ Target } alt="target"/>
-      <div className="card-small">
-        { idea.body }
+        <div className="card-content-small">
+        <Link to={'/' + idea.id}>
+      <span className="card-title purple-text">{ idea.body }</span>
+      </Link>
         <br/>
         <button onClick={() => this.props.deleteIdea(idea.id)}>bad idea</button>
       </div>
