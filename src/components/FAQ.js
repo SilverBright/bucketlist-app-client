8fa9252a1f6fb1967d6b1047c4379a43f6506033
github username <github email address>
2019-06-02 17:44:03 -0700

    stub out faq.js

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -4,7 +4,7 @@ const faq = () => {
   return (
     <div className="container">
       <h3 className="center">FAQ</h3>
-      <h5>This is my wild and wacky bucketlist.</h5>
+      <h5>According to <a href="https://www.merriam-webster.com/dictionary/bucket%20list">Merriam-Webster</a>, a bucket list is a list of things to do that one has not done before but wants to do before dying.</h5>
     </div>
   )
 }
