66683a9b56f4c8998285f4b80941e438bbddf798
github username <github email address>
2019-06-02 17:31:05 -0700

    add FAQ page and link to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/FAQ">FAQ</Link></li>
         </ul>
       </div>
     </nav>
