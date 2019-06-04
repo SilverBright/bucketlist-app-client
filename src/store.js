495b591cf432d865a47594692b42028affad0976
github username <github email address>
2019-06-03 22:36:56 -0700

    delete unused code

--- src/store.js
+++ src/store.js
@@ -1,31 +1,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk' 
 import appTransactions from './reducers/appTransactions';
-// import currentIdea from './reducers/currentIdea';
-// import ideaFormData from './reducers/ideaFormData';
 import ideas from './reducers/ideas';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 
-// combine all the reducers
+// combine reducers
 const reducers = combineReducers({
   appTransactions,
-  // currentIdea,
-  // ideaFormData,
-  ideas,
-})
+  ideas
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-// const middleware = [ReduxThunk];
-
-// the store manages all of the state
-
-// const store = createStore(
-//   reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE && window.__REDUX_DEVTOOLS_EXTENSION__(),
-//   applyMiddleware(ReduxThunk)
-//   )
 
 const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
 
