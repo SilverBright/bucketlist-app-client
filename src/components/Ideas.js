6feb0f23be0537dc66c98f98e211786ef62dbd53
github username <barbara.m.washington@gmail.com>
2019-06-18 22:13:26 -0700

    create handleClick function with console.log for testing

--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -9,6 +9,10 @@ class Ideas extends Component {
     this.props.getIdeas()
   }
 
+  handleClick = () => {
+    console.log("button clicked!")
+  }
+
 render() {
   const renderIdeas = this.props.ideas.map(idea =>
     <div className="bucketlist card" key={idea.id}>
@@ -21,40 +25,40 @@ render() {
   </div>
 ).reverse()
 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
   const sortedIdeas = this.props.ideas.sort((i,j) => i.body.localeCompare(j.body, {sensitivity: 'base'}))
-  console.log(sortedIdeas)
+  // console.log(sortedIdeas)
 
-  const renderSortedIdeas = this.props.ideas.map(idea =>
+  const renderSortedIdeas = sortedIdeas.map(idea =>
     <div className="bucketlist card" key={idea.id}>
       <img src={ Target } alt="target"/>
         <div className="card-content-small">
-      <span className="card-title blue-text text-darken-2">{ idea.body }</span>
-      <br/>
-      <button onClick={() => this.props.deleteIdea(idea.id)}>completed</button>
+        <span className="card-title blue-text text-darken-2">{ idea.body }</span>
+        <br/>
+        <button onClick={() => this.props.deleteIdea(idea.id)}>completed</button>
+      </div>
     </div>
-  </div>
-);
- 
-  // const sortedIdeas = this.props.ideas.sort(function (a, b) {
-  //  const bodyA = a.body.toUpperCase();
-  //  const bodyB = b.body.toUpperCase();
-
-  //   if (bodyA < bodyB) 
-  //   return -1;
-  //   else if (bodyA > bodyB ) return 1;
-
-  //   return 0;
-  // });
-  // console.log(sortedIdeas);
-
+  );
+    
   return (
     <div>
-      <button>sort the list alphabetically</button> 
-      {/* { renderIdeas } */}
-      { renderSortedIdeas }
+      <button onClick={this.handleClick}>sort the list alphabetically</button> 
+      { renderIdeas }
+      {/* { renderSortedIdeas } */}
     </div>
     )
   }
 }
 
 export default connect(state => ({ ideas: state.ideas }), { getIdeas, deleteIdea })(Ideas);
+
+
+  // const sortedIdeas = this.props.ideas.sort(function (a, b) {
+  //   const bodyA = a.body.toUpperCase();
+  //   const bodyB = b.body.toUpperCase();
+  //    if (bodyA < bodyB) 
+  //    return -1;
+  //    else if (bodyA > bodyB ) return 1;
+  //    return 0;
+  //  });
+  //  console.log(sortedIdeas);
