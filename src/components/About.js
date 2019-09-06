f8330899afc724a4d834774022b1638c98bfb0f7
github username <barbara.m.washington@gmail.com>
2019-09-05 17:06:19 -0700

    fix bulleting issue with Materializecss styling on About page

--- src/components/About.js
+++ src/components/About.js
@@ -3,19 +3,18 @@ import React from 'react';
 const About = () => {
   return (
     <div className="container"><br />
-      <h3 className="center">About</h3>
-      <h5>Welcome to my React on Rails app: Bucket List</h5>
-        <div className="container">
-        <h5>Bucket List is a simple todo style app built with:
-          <p><ul className="container browser-default left-align">
-              <li>React/Redux-Thunk frontend</li>
-              <li>Rails API backend</li>
-              <li>React-router for navigation</li>
-              <li>and, an optional button to toggle between sorting the list alphabetically, or in reverse order</li>
-            </ul>
-          </p>
-        </h5>
-      </div>
+      <h3>About</h3>
+      <h5>Welcome to my React on Rails app, Bucket List. Bucket List is a todo style app built with:</h5>
+        <div className="container left-align">
+          <ul className="browser-default">
+            <li>React front end</li>
+            <li>Redux & Redux Thunk for store management and middleware</li>
+            <li>Rails API backend for data persistence</li>
+            <li>React-router for navigation</li>
+            <li>Materializecss for styling</li>
+            <li>and, an optional button to toggle between sorting your list alphabetically, or in sorting reverse order</li>
+          </ul>
+        </div>
     </div>
   )
 }
