51f169bdc056a9fbaf479e73587772dfd039ddcf
github username <barbara.m.washington@gmail.com>
2019-09-06 17:10:35 -0700

    fix html headings and layout

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,13 +6,13 @@ const faq = () => {
       <h2 className="center">FAQ</h2>
 
       <h4>What is a bucket list?</h4>
-      <h5>According to <a href="https://www.merriam-webster.com/dictionary/bucket%20list">Merriam-Webster</a>, a bucket list is a list of things to do that one has not done before, but wants to do before dying.</h5>
+      <p className="left-align">According to <a href="https://www.merriam-webster.com/dictionary/bucket%20list">Merriam-Webster</a>, a bucket list is a list of things to do that one has not done before, but wants to do before dying.</p><br />
       
-      <h4>Why is it called a bucket list?</h4>
-      <h5>According to <a href="http://www.macmillandictionaryblog.com/bucket-list">MacMillan Dictionary</a>, The term bucket list is a relatively new one, popularized by a movie of the same name starring American actors Jack Nicholson and Morgan Freeman released in the early 21st century. Bucket list comes from the phrase ‘to kick the bucket’ which is a figure of speech meaning ‘to die’.</h5>
+      <h4 >Why is it called a bucket list?</h4>
+      <p className="left-align">According to <a href="http://www.macmillandictionaryblog.com/bucket-list">MacMillan Dictionary</a>, The term bucket list is a relatively new one, popularized by a movie of the same name starring American actors Jack Nicholson and Morgan Freeman released in the early 21st century. Bucket list comes from the phrase ‘to kick the bucket’ which is a figure of speech meaning ‘to die’.</p><br />
       
       <h4>Where does the term 'to kick the bucket' come from?</h4>
-      <h5>Essentially, it is an expression of death. There are multiple, fascinating theories about how this originated. A good reference can be found on <a href ="https://en.wikipedia.org/wiki/Kick_the_bucket">wikipedia.</a></h5>
+      <p className="left-align">Essentially, it is an expression of death. There are multiple, fascinating theories about how this originated. A good starting reference can be found on <a href ="https://en.wikipedia.org/wiki/Kick_the_bucket">wikipedia.</a></p>
     </div>
   )
 }
