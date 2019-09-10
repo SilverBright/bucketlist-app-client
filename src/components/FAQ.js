e272a43b318c84b70362cf869d08f86366e077df
github username <barbara.m.washington@gmail.com>
2019-09-09 22:45:39 -0700

    fix character length for lines with longer code

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -6,15 +6,26 @@ const faq = () => {
       <h2 className="center">FAQ</h2>
 
       <h4>What is a bucket list?</h4>
-      <p className="left-align">According to <a href="https://www.merriam-webster.com/dictionary/bucket%20list">Merriam-Webster</a>, a bucket list is a list of things to do that one has not done before, but wants to do before dying.</p><br />
+      <p className="left-align">According to 
+      <a href="https://www.merriam-webster.com/dictionary/bucket%20list">Merriam-Webster</a>, 
+      a bucket list is a list of things to do that one has not done before, 
+      but wants to do before dying.</p><br />
       
       <h4 >Why is it called a bucket list?</h4>
-      <p className="left-align">According to <a href="http://www.macmillandictionaryblog.com/bucket-list">MacMillan Dictionary</a>, The term bucket list is a relatively new one, popularized by a movie of the same name starring American actors Jack Nicholson and Morgan Freeman released in the early 21st century. Bucket list comes from the phrase ‘to kick the bucket’ which is a figure of speech meaning ‘to die’.</p><br />
+      <p className="left-align">According to 
+      <a href="http://www.macmillandictionaryblog.com/bucket-list">MacMillan Dictionary</a>, 
+      The term bucket list is a relatively new one, popularized by a movie of the same name,
+      starring Jack Nicholson and Morgan Freeman, released in the early 21st century. 
+      Bucket list comes from the phrase ‘to kick the bucket’,
+      which is a figure of speech meaning ‘to die’.</p><br />
       
       <h4>Where does the term 'to kick the bucket' come from?</h4>
-      <p className="left-align">Essentially, it is an expression of death. There are multiple, fascinating theories about how this originated. A good starting reference can be found on <a href ="https://en.wikipedia.org/wiki/Kick_the_bucket">wikipedia.</a></p>
+      <p className="left-align">Essentially, it is an expression of death. 
+      There are multiple, fascinating theories about how this originated. 
+      A good starting reference can be found on 
+      <a href ="https://en.wikipedia.org/wiki/Kick_the_bucket">wikipedia.</a></p>
     </div>
   )
 }
 
-export default faq;
+export default faq;                                                            
--- src/components/Ideas.js
+++ src/components/Ideas.js
@@ -22,19 +22,24 @@ render() {
   let sortedIdeas 
     if (this.state.isSorted === true) {
       // Ideas sorted alphabetically
-      sortedIdeas = this.props.ideas.concat().sort((i,j) => i.body.localeCompare(j.body, {sensitivity: 'base'}));
+      sortedIdeas = this.props.ideas.concat().sort((i, j) => i.body.localeCompare(j.body, {
+        sensitivity: 'base'
+    }));
     } else {
       // Ideas sorted by creation date
-      sortedIdeas = this.props.ideas.concat().sort((i,j) => j.created_at.localeCompare(i.created_at, {sensitivity: 'base'}));
+      sortedIdeas = this.props.ideas.concat().sort((i, j) => j.created_at.localeCompare(i.created_at, {                
+        sensitivity: 'base'
+    }));
   }
 
  const renderIdeas = sortedIdeas.map(idea =>
     <div className="bucketlist card" key={idea.id}>
       <img src={ Target } alt="target"/>
-        <div className="card-content-small">
-          <span className="card-title blue-text text-darken-2">{ idea.body }</span>
-        <br/>
-        <button class="waves-effect btn-small red darken-4"onClick={() => this.props.deleteIdea(idea.id)}>click to complete</button>
+      <div className="card-content-small">
+        <span className="card-title blue-text text-darken-2">{ idea.body }</span><br/>
+        <button class="waves-effect btn-small red darken-4"
+         onClick={() => this.props.deleteIdea(idea.id)}>click to complete
+        </button>
       </div>
     </div>
   )
